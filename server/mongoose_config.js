54a051eec4abda4b96a4c945d0418610af79fdc9
Mikkel <mikkel.fjeld@tutanota.com>
2018-06-14 17:30:07 +0200

    ok

--- server/mongoose_config.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.connect('mongodb://mikkel:Passord123@ds259410.mlab.com:59410/gql-library')
-mongoose.connection.once('open', () => {
-    console.log("yo")
-})
-
-module.exports = mongoose
